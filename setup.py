2e59f8c3d3c235a6345afc6909200b19397c76a3
Chris Maue <csmaue@gmail.com>
2014-11-19 16:05:05 -0600

    Merged into fewer files

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 try:
-	from setuptools import setup
+    from setuptools import setup
 except ImportError:
-	from distutils.core import setup
+    from distutils.core import setup
 
 config = {
-	'description': 'Generate GUI from INI',
-	'author': 'Chris Maue',
-	'author_email': 'csmaue@gmail.com',
+    'description': 'Generate GUI from INI',
+    'author': 'Chris Maue',
+    'author_email': 'csmaue@gmail.com',
     'url': 'https://github.com/csm0042/guibyini.git',
     'download_url': 'https://github.com/csm0042/guibyini.git',
-	'version': '1.0.0',
-	'packages': ['guibyini'],
-	'name': 'guibyini'
+    'version': '1.0.0',
+    'packages': ['guibyini'],
+    'name': 'guibyini'
 }
 setup(**config)
